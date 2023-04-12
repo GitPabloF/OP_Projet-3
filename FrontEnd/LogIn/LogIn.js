ceb3a756cd3c1065aec8eddac39747746143edd3
UserWindowsXP <wwwebsurf@gmail.com>
2023-04-12 16:39:41 +0200

    LogIn page v1 - admin mode v1

--- FrontEnd/LogIn/LogIn.js
+++ FrontEnd/LogIn/LogIn.js
@@ -1,46 +1,79 @@
-async function logUser() {
+// 2.2 : Authentification de l’utilisateur 
+let utilisateurEstConnecte = false;
 
-    // séléction de la partie le btn submit + ajout de l'event listener 
+async function logUser() {
+    // 2.2.1 séléction de la partie le btn submit + ajout de l'event listener 
     const btnEnvoyer = document.querySelector('form');
 
-    btnEnvoyer.addEventListener("submit", (event) => {
-        // ajouter le preventDefault();
-        console.log("testOk")
+    btnEnvoyer.addEventListener("submit", async (event) => {
+        // 2.2.2 ajouter le preventDefault();
         event.preventDefault();
 
-        // Créer la charge utile 
+        // 2.2.3 créer la charge utile 
         const connexion = {
             email: event.target.querySelector("[name=e-mail]").value,
             password: event.target.querySelector("[name=mot-de-passe]").value
         };
 
-        // mettre la charge utilie au format JSON string
+        // 2.2.4 mettre la charge utilie au format JSON string
         let chargeUtile = JSON.stringify(connexion);
 
-        const res =  fetch('http://localhost:5678/api/users/login', {
+        try {
+
+            // 2.2.5 appel de fetch en POST 
+            const reponse = await fetch('http://localhost:5678/api/users/login', {
                 method: "post",
                 headers: { "content-type": "application/json" },
                 body: chargeUtile
-        })
-            .then(()=> {
-                if (chargeUtile.ok) {
-                    // Créer fetch en POST / Pourquoi on ne mets pas await
-                    console.log("utilisateur connecté")
-        
-                    // Re dériger vers mode édition
-                    
-                }else{
-                    console.log("une erreur est survenu")
-                    // message erreur 
-                }
+            })
 
-            }); 
+            if (reponse.ok) {
+                // Créer fetch en POST / Pourquoi on ne mets pas await
+                const res = await reponse.json();
+                console.log("utilisateur connecté");
+                console.log(res);
+                console.log(reponse);
+            
+                //Stoker le token 
+                window.localStorage.setItem("token",res.token);
 
-        
+                utilisateurEstConnecte = true;
+                console.log(utilisateurEstConnecte);
+                // Re dériger vers mode édition
+                window.open("../index.html");
+                
 
-    })
+            } else{
+                console.log("une erreur est survenu"); 
+                // message erreur 
+                const err_serveurSelector = document.querySelector("#Erreur_Serveur");
+                err_serveurSelector.style.display = "none";
+
+                const logInSelector = document.querySelector("#LogIn_invalide");
+                logInSelector.style.display = "block";
+
+                const inputSelector = document.querySelector("#e-mail");
+                const inputSelector2 = document.querySelector("#mot-de-passe");
+                inputSelector.style.border = "solid 0.5px #d65757";
+                inputSelector2.style.border = "solid 0.5px #d65757";
+
+                utilisateurEstConnecte = false;
+
+            } 
 
 
+        } catch (error) {
+            const logInSelector = document.querySelector("#LogIn_invalide");
+            logInSelector.style.display = "none";
+
+            const err_serveurSelector = document.querySelector("#Erreur_Serveur");
+            err_serveurSelector.style.display = "block";
+
+            console.log("serveur")
+
+        }
+
+    });
 
 }
 
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,58 +1,101 @@
-async function getData() {
-        // 1.1 récupérer les travaux du back-end 
+function getData() {
+    // 1.1 récupérer les travaux du back-end 
     try {
         // 1.1.1 récup les data de l'API
-        const donnees =  fetch('http://localhost:5678/API/works')
-            .then(donnees => donnees.json());
-        
+        fetch('http://localhost:5678/API/works')
+            .then(donnees => donnees.json())
 
-        // 1.1.2 Via une boucle for, supprimer et afficher les nouveaux contenus 
+            // 1.1.2 afficher les projets 
+            .then(donnees => {
+                function genererElements(donnees) {
+                    // 1.1.3 retire les projes de la page au lancement de la fonction
+                    document.querySelector('#portfolio .gallery').innerHTML = "";
+                    // 1.1.4 boucle for qui créé les éléments HTML et incorpore les éléments de l'API
+                    for (let i = 0; i < donnees.length; i++) {
+                        document.querySelector('#portfolio .gallery').innerHTML +=
+                            `<figure> <img src="${donnees[i].imageUrl}" alt="${donnees[i].title}"> <figcaption> ${donnees[i].title}</figcaption> </figure>`;
+                    }
+                }
+                genererElements(donnees);
 
-        function genererElements(donnees) {
-            document.querySelector('#portfolio .gallery').innerHTML = "";
+                // 1.2 Réalisation des filtres   
+                // 1.2.1 Séléctionner les btn du HTML 
+                const btnTous = document.querySelector('#portfolio #tous');
+                const btnObjet = document.querySelector('#portfolio #objets');
+                const btnAppartements = document.querySelector('#portfolio #appartements');
+                const btnHotelsRestaurants = document.querySelector('#portfolio #hotels_restaurants');
 
-            for (let i = 0; i < donnees.length; i++) {
+                // 1.2.2 Création de d'une fonction avec une fonction filter 
+                function filtrage(nomFiltre) {
+                    const elementsFiltrees = donnees.filter((donnee) => {
+                        return donnee.category.name == `${nomFiltre}`;
+                    });
+                    // 2.2.3 Retirer tous les éléments du HTML + appeler la fonction avec les nouveaux élements filtres
+                    document.querySelector('#portfolio .gallery').innerHTML = "";
+                    genererElements(elementsFiltrees);
+                }
 
-                document.querySelector('#portfolio .gallery').innerHTML +=
-                    `<figure> <img src="${donnees[i].imageUrl}" alt="${donnees[i].title}"> <figcaption> ${donnees[i].title}</figcaption> </figure>`;
+                //1.2.4 ajouter l'écouteur avec la fonction de filtrage avce l'élément souhaité
 
-            }
-        }
+                btnTous.addEventListener("click", () => genererElements(donnees));
+                btnObjet.addEventListener("click", () => filtrage("Objets"));
+                btnAppartements.addEventListener("click", () => filtrage("Appartements"));
+                btnHotelsRestaurants.addEventListener("click", () => filtrage("Hotels & restaurants"));
 
-        genererElements(donnees);
-        // 1.2 Réaliser les filtres 
-        // 1.2.1 Séléctionner les btn du HTML -
-
-        const btnTous = document.querySelector('#portfolio #tous');
-        const btnObjet = document.querySelector('#portfolio #objets');
-        const btnAppartements = document.querySelector('#portfolio #appartements');
-        const btnHotelsRestaurants = document.querySelector('#portfolio #hotels_restaurants');
-        
-
-        // 1.2.2 Création de d'une fonction avec une fonction filter 
-
-        function filtrage(nomFiltre) {
-            const elementsFiltrees = donnees.filter((donnee) => {
-                return donnee.category.name == `${nomFiltre}`;
-            });
-            // 2.2.3 Retirer tous les éléments du HTML + appeler la fonction avec les nouveaux élements filtres
-            document.querySelector('#portfolio .gallery').innerHTML = "";
-            genererElements(elementsFiltrees);
-        }
-        
-        //1.2.4 ajouter l'écouteur avec la fonction de filtrage avce l'élément souhaité
-        
-        btnTous.addEventListener("click", () => genererElements(donnees));
-        btnObjet.addEventListener("click", () => filtrage("Objets"));
-        btnAppartements.addEventListener("click", () => filtrage("Appartements"));
-        btnHotelsRestaurants.addEventListener("click", () => filtrage("Hotels & restaurants"));
-        
-
+            })
     } catch (error) {
         console.error('Une erreur est survenue', error);
     }
 };
 
 getData();
+
+// mode édition 
+
+// début post avec token
+
+
+// fin post avec token 
+
+if (localStorage.getItem("token") != null) {
+    console.log("token user ok")
+    // afficher /masquer les éléments 
+    const editionSelecteur = document.querySelector("#edition");
+    editionSelecteur.style.display = "flex";
+
+    const headerSelecteur = document.querySelector("header");
+    headerSelecteur.style.margin = "87px 0";
+
+    const navLoginSelecteur = document.querySelector("#nav-login");
+    navLoginSelecteur.textContent = "logout"
+
+    const filtresSelecteur = document.querySelector("#filtres");
+    filtresSelecteur.style.display = "none";
+
+    const portfolioh2Selecteur = document.querySelector("#portfolio h2");
+    portfolioh2Selecteur.style.margin = "3em";
+
+    // --- bouton logout ------
+
+    // selectionner le bouton 
+    const logoutSelecteur = document.querySelector("#nav-login");
+
+    // ajouter un écouteur 
+    logoutSelecteur.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("test logout ok");
+        // au click -> clearlocalstorage + refraichir la page 
+        window.localStorage.removeItem("token");
+        document.location.reload();
+        // rafraichir la page 
+    });
+
+}
+
+// modale 
+
+
+
+
 
 
